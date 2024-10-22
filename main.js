43380aab971b74a29aa0a2107fe3dd94a55aa234
Rupraj Singh <ruprajsingh1@gmail.com>
2024-10-22 23:31:20 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      // preload: path.join(__dirname, 'preload.js') // if you have a preload script
+    },
+  });
+
+  // mainWindow.loadFile(path.join(__dirname, "./my-app/build/index.html"));
+  mainWindow.loadURL("http://localhost:3000");
+
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) createWindow();
+});
